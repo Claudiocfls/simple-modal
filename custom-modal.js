01ba810ac0459401497fef6ba8ead313571969c8
claudio.silva <claudio.silva@loft.com.br>
2021-11-30 23:15:30 -0300

    feat: add interactivity for modal

--- custom-modal.js
+++ custom-modal.js
@@ -54,10 +54,43 @@ class Modal extends HTMLElement{
     this._optionClicked = null;
     this.attachShadow({ mode: 'open'});
     this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.shadowRoot.querySelectorAll('.modal-button')
+      .forEach((button) => {
+        button.addEventListener("click", this.handleClickOnButton.bind(this));
+      });
+  }
+
+  static get observedAttributes() {
+    return ['open'];
+  }
+
+  handleClickOnButton(evt) {
+    const optionClicked = evt.target.innerText;
+    this.setAttribute('open', 'false');
+    this.emitCustomEventWithOption(optionClicked);
+  }
+
+  emitCustomEventWithOption(optionClicked) {
+    const customEvent = new CustomEvent('modal-option-clicked', {
+      bubbles: true,
+      composed: true,
+      detail: {
+        option: optionClicked,
+      }
+    });
+
+    this.dispatchEvent(customEvent);
   }
 
   connectedCallback() {
     this.shadowRoot.querySelector('#modal-message').innerText = this.getAttribute('message');
+    this.shadowRoot.querySelector('.modal-background').style.display = this.getAttribute('open') === 'true' ? 'flex' : 'none';
+  }
+
+  attributeChangedCallback(attrName, _, newVal) {
+    if (attrName === 'open') {
+      this.shadowRoot.querySelector('.modal-background').style.display = newVal === 'true' ? 'flex' : 'none';
+    }
   }
 }
 
